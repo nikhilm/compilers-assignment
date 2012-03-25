874fecf773c9a2503a3c12117df38c8e2f4f430c
Shreya agrawal <shreya_68@hotmail.com>
2012-03-25 13:47:12 +0530

    NFA to DFA added

--- regex.c
+++ regex.c
@@ -11,30 +11,60 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <ctype.h>
 
+// include the linked list utilities written by someone else
+// a struct with a 'next' member can act as a singly linked list
+// while a struct with 'prev' and 'next' is doubly linked
 #include "utlist.h"
 
+// because we are only dealing with ascii
 #define ALPHABET_SIZE 128
+#define NO_OF_STATES 128
 
+// this defines what an NFA state is supposed to be
 typedef struct NFAState {
+    // for ASCII character with code X, transitions[X] is the next state(s)
+    // see the last member 'next'
+    // 0 position is a epsilon transition
     struct NFAState *transitions[ALPHABET_SIZE]; // yes it's a colossal waste of space,
-                                                 // 0 position is a epsilon transition
+
+    // 1 if this is an accepting state
     int is_final;
+
+    // DOT is a file format to describe graphs,
+    // which graphviz will read and generate the pretty graphs
+    // this is just a number, and then a global counter is
+    // incremented to assign each NFAState a unique name
+    // this is not important to the core algorithm
     int dot_name; // DOT graph node name
+
+    // again, have we visited this DOT node when rendering
+    // not important to algorithm
     int visited;  // DOT rendering visited?
+
+    // Every transition is actually a linked list of NFA states
+    // because by definition an NFA can go to multiple states at
+    // the same time.
     struct NFAState *next; // so that transitions can be a linked list
                            // transitions[0] is a list of states to which it
                            // can go
 } NFAState;
 
+// TODO: This is something you'll have to fix,
+// it probably isn't supposed to look like this
+// since a DFAState hold's an amalgamation of NFAStates
+// and so needs some way to hold various states in it
 typedef struct DFAState_t {
     struct DFAState *transitions[ALPHABET_SIZE]; // yes it's a colossal waste of space,
-                                                 // 0 position is a epsilon transition
+    NFAState nfa_states[NO_OF_STATES];
+    int no_of_nfa_states;                                                 // 0 position is a epsilon transition
     int is_final;
     int dot_name; // DOT graph node name
     int visited;  // DOT rendering visited?
 } DFAState;
 
+// abstraction to malloc an NFAState
 NFAState *NFAState_create()
 {
     NFAState *state = (NFAState*) malloc(sizeof(NFAState));
@@ -44,14 +74,17 @@ NFAState *NFAState_create()
     return state;
 }
 
+// free an NFAState
 void NFAState_delete(NFAState *state)
 {
     free(state);
 }
 
+// TODO: again modify this based on your implementation
 DFAState *DFAState_create()
 {
     DFAState *state = (DFAState*) malloc(sizeof(DFAState));
+    state->no_of_nfa_states = 0;
     state->is_final = 0;
     state->dot_name = 0;
     return state;
@@ -65,11 +98,16 @@ void DFAState_delete(DFAState *state)
 /*************************************
  * Regex infix to postfix conversion *
  ************************************/
+
+//a doubly linked list is used for 
+//holding the postfix form of the expression
+//value is the current character
 typedef struct postfix_expr {
     char value;
     struct postfix_expr *next, *prev;
 } postfix_expr;
 
+// similar to NFAState_create
 postfix_expr *postfix_expr_create(char value)
 {
     postfix_expr *item = (postfix_expr*) malloc(sizeof(postfix_expr));
@@ -82,6 +120,10 @@ void postfix_expr_delete(postfix_expr *exp)
     free(exp);
 }
 
+// abstracts the task of dealing with when to insert concatenation
+// you don't need to worry about this, since you will only
+// deal with NFAState, let's just say that it works and i'll explain it
+// later
 postfix_expr * push_op(postfix_expr *output, postfix_expr *opstack, char op)
 {
     // handle duplicate '.'
@@ -106,7 +148,9 @@ postfix_expr * push_op(postfix_expr *output, postfix_expr *opstack, char op)
     return opstack;
 }
 
-/* caller should free */
+// returns a DL list containing the postfix expression given an infix
+// expression
+// caller should free the returned list
 postfix_expr * postfix(const char *infix)
 {
     postfix_expr *output = NULL;
@@ -162,11 +206,17 @@ postfix_expr * postfix(const char *infix)
 /*******************************
  * Thompson core and utilities *
  ******************************/
+// holds the start and end nodes of a particular transition diagram
+// since Thompson is all about hooking up start and end of the three
+// basic types, we use this to simplify things.
 typedef struct StartAndEnd {
     NFAState *start;
     NFAState *end;
 } StartAndEnd;
 
+int inputs(ALPHABET_SIZE);
+int no_of_inputs = 0;
+// epsilon -> char -> epsilon
 StartAndEnd single(char value)
 {
     NFAState *start = NFAState_create();
@@ -241,6 +291,28 @@ StartAndEnd star(StartAndEnd one)
     return d;
 }
 
+void add_to_input(char value)
+{
+    int i;
+    for( i = 0; i < no_of_inputs; i++)
+    {
+        if(inputs[i] == (int)value)
+            break;
+    }
+    if( i == no_of_inputs )
+    {
+        inputs[no_of_inputs] = (int)value;
+        no_of_inputs++;
+    }
+}
+
+// this is used to convert a postfix expression
+// to the final complete NFA
+// stack starts empty
+// any basic NFA form is put on the stack as
+// its start and end pair
+// on an operation, there is an n-ary pop depending
+// on the arity of the operation and then result is pushed
 typedef struct NFAStack {
     StartAndEnd nfa;
     struct NFAStack *next;
@@ -253,6 +325,7 @@ NFAStack *NFAStack_create(StartAndEnd value)
     return item;
 }
 
+// core Thompson algorithm
 NFAState *thompson(const char *pattern)
 {
     postfix_expr *p = postfix(pattern);
@@ -286,6 +359,7 @@ NFAState *thompson(const char *pattern)
                 break;
             default:
                 new_top = NFAStack_create(single(el->value));
+                add_to_input(el->value);
                 LL_PREPEND(stack, new_top);
                 break;
         }
@@ -296,10 +370,117 @@ NFAState *thompson(const char *pattern)
     return stack->nfa.start;
 }
 
+// converting NFA to DFA
+
+int find_closure(DFAState *dfa_node)
+{
+   NFAState *n;
+   NFAState *nfa_node
+   int i;
+   for( i = 0; i < dfa_node->no_of_nfa_states;i++)
+   {
+       nfa_node = dfa_node->nfa_states[i];
+
+       LL_FOREACH(nfa_node->transitions[0], n)
+       {
+           if(n != NULL)
+           {
+               dfa_node->nfa_states[no_of_nfa_states] = n;
+               dfa_node->no_of_nfa_states++;
+               find_closure(dfa_node, n);
+            }
+           else
+               return 0;
+        }
+    }
+   return 1;
+}
+
+DFAState add_states(DFAState *node)
+{
+    int i,j,x;
+    NFAState *n;
+    for( i = 0; i < no_of_inputs; i++)
+    {
+        input = inputs[i];
+        x = node->no_of_nfa_states
+        for( j = 0; j < x; j++)
+        {
+            LL_FOREACH( node->nfa_states[j]->transitions[input], n)
+            {
+                if( n == NULL )
+                    break;
+                else
+                {
+                    node->nfa_states[node->no_of_nfa_states] = n;
+                    node->no_of_nfa_states++;
+                }
+            }
+        }
+    }
+
+
+typedef struct DFAStack {
+    DFAState *dfa; 
+    struct DFAStack *next;
+} DFAStack;
+
+DFAStack *DFAStack_create(DFAState *dfa)
+{
+    DFAStack *item = (DFAStack*) malloc(sizeof(DFAStack));
+    item->dfa = value;
+    return item;
+}
+
+DFAState nfa_to_dfa(NFAState *nfa_start)
+{
+    DFAStack *stack = NULL;
+    DFAStack *top = NULL;
+    DFAStack *new_states = NULL;
+    DFAStack *new_top = NULL:
+    DFAState *start = DFAState_create();
+    //DFAState *end = DFAState_create();
+    //end->is_final = 1;
+    start->nfa_states[start->no_of_nfa_states] = nfa_start;
+    start->no_of_nfa_states++;
+    find_closure(start);
+    new_top =  DFAStack_create(start);
+    LL_PREPEND(new_states, new_top);
+    int i,j,x;
+    NFAState *n;
+    LL_FOREACH(new_states, n)
+    {
+        for( i = 0; i < no_of_inputs; i++)
+        {
+            DFAState *new_state = DFAState_create();
+            input = inputs[i];
+            x = n->no_of_nfa_states
+            for( j = 0; j < x; j++)
+            {
+                LL_FOREACH( n->nfa_states[j]->transitions[input], n)
+                {
+                    if( n == NULL )
+                        break;
+                    else
+                    {
+                        new_state->nfa_states[node->no_of_nfa_states] = n;
+                        new_state->no_of_nfa_states++;
+                    }
+                }
+            }
+            find_closure(new_state);
+        }
+        add_states(start); 
+
+}
+
 /******************
  * DOT generation *
  *****************/
+// the global counter for assigning DOT node labels
 int dot_name_counter = 0;
+// output a DOT description of an NFAState,
+// you'll write something similar for DFA
 void dot_node(NFAState *node)
 {
     if (node->dot_name == 0) {
@@ -309,32 +490,61 @@ void dot_node(NFAState *node)
     // we've already output this node, skip
 }
 
+// output a DOT description of a transition
 void dot_edge(NFAState *from, NFAState *to, char label)
 {
     printf("node%d -> node%d [label=", from->dot_name, to->dot_name);
-    if (label == '\0')
+    if (label == '\0') {
         printf("epsilon");
-    else
+    }
+    else if(isspace(label)) {
+        switch (label) {
+            case ' ':
+                printf("sp");
+                break;
+            case '\t':
+                printf("tab");
+                break;
+            case '\n':
+                printf("nl");
+                break;
+            case '\r':
+                printf("cr");
+                break;
+            default:
+                printf("whitespace");
+        }
+    }
+    else {
         printf("%c", label);
+    }
     printf("]\n");
 }
 
+// recursively output a complete NFA
 void dot_nfa_rec(NFAState *state)
 {
+    // ignore visited
     if (state->visited)
         return;
+    // output node
     dot_node(state);
+    // now visited
     state->visited = 1;
     int i;
+    // follow all transitions and output them
     for (i = 0; i < ALPHABET_SIZE; ++i) {
         NFAState *each;
         LL_FOREACH(state->transitions[i], each) {
             dot_nfa_rec(each);
+            // draw an edge from state to this transition
             dot_edge(state, each, (char) i);
         }
     }
 }
 
+// prints out DOT prologue and epilogue
+// then calls the recursive algorithm
 void dot_nfa(NFAState *start)
 {
     printf("digraph nfa {\nsize=\"16,16\";\n");
@@ -355,7 +565,7 @@ int main(int argc, char **argv)
     char *pattern = argv[1];
     NFAState *start_nfa = thompson(pattern);
     dot_nfa(start_nfa);
-    //DFAState start_dfa = nfa_to_dfa(start_nfa);
+    DFAState start_dfa = nfa_to_dfa(start_nfa);
     //graphviz(start_dfa);
     return 0;
 }
